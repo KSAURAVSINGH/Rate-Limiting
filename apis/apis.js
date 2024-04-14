5c1648a7d85abe5ab49801624ae7f8682226b7b9
KUMAR SAURAV <kumarsauravsingh1234567890@gmail.com>
2024-04-14 19:41:23 +0530

    rateLimit - tokenbucket technique

--- apis/apis.js
+++ apis/apis.js
@@ -1,9 +1,9 @@
 const welcomeComp = require('../routes/components/welcome')
-const {customRateLimiting} = require('../middlewares/rateLimit')
+const {customRedisTokenBucketRateLimiting} = require('../middlewares/rateLimit')
 
 const handleApis = (app) =>{
-    app.get('/', customRateLimiting, welcomeComp.welcomeComp);
-    app.get('/data', customRateLimiting, welcomeComp.fetchData);
+    app.get('/', customRedisTokenBucketRateLimiting, welcomeComp.welcomeComp);
+    app.get('/data', welcomeComp.fetchData);
 }
 
 module.exports = handleApis;
--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -8,11 +8,12 @@ redisClient.on('error', (err)=>console.error('Redis client error: ', err));
     await redisClient.connect();
 })();
 
-const WINDOW_SIZE_IN_MINS = 2;
-const MAX_WINDOW_REQUEST_COUNT = 10;
-const WINDOW_LOG_INTERVAL_IN_MINS = 1;
+const MAX_TOKENS_ALLOWED = 10
+const TOKENS_TO_BE_FILLED = 2
+const TOKENS_FILLED_PER_IN_SECONDS = 10
+const TOKEN_BUCKET_REDIS_KEY = 'tokenbucket'
 
-const customRateLimiting = async (req, res, next) =>{
+const customRedisTokenBucketRateLimiting = async (req, res, next) =>{
     
     try{
         // check that redis client exists
@@ -21,61 +22,58 @@ const customRateLimiting = async (req, res, next) =>{
         }
         
         // fetch records for the current ip address from the redis
-        const recordData = await redisClient.get(req.ip);
+        const recordData = await redisClient.get(TOKEN_BUCKET_REDIS_KEY);
+        // tokenbucket key will have an object as a value with tokensInUse and requestTimeStamp as parameters
+    
         const currentRequestTime = moment();
-        console.log("Records Available: ", recordData);
+        console.log("Record: ", recordData);
         console.log("Current Request Time: ", currentRequestTime);
 
-        if(recordData == null){
-            let recordList = [];
+        if(recordData == null){            
             let newRecord = {
                 requestTimeStamp: currentRequestTime.unix(),
-                requestCount: 1,
+                tokensInUse: 1,
               }
-            recordList.push(newRecord);
-            await redisClient.set(req.ip, JSON.stringify(recordList));
+            
+            await redisClient.set(TOKEN_BUCKET_REDIS_KEY, JSON.stringify(newRecord));
             next();
         }
         else{
             // if recordData is found, parse it's value and calculate number of requests users has made within the last window
             let data = JSON.parse(recordData);
-            let windowStartTimestamp = moment().subtract(WINDOW_LOG_INTERVAL_IN_MINS, 'minutes').unix();
+            let windowStartTimestamp = moment().subtract(TOKENS_FILLED_PER_IN_SECONDS, 'seconds').unix();
             
-            let requestsWithinWindow = data.filter((entry) => {
-            return entry.requestTimeStamp > windowStartTimestamp;
-            });
+            let requestsStartTimeBasedOnRecord = data.requestTimeStamp
 
-            let totalWindowRequestsCount = requestsWithinWindow.reduce((accumulator, entry) => {
-                return accumulator + entry.requestCount;
-            }, 0);
-
-            if(totalWindowRequestsCount>MAX_WINDOW_REQUEST_COUNT){
-                res.status(429).send(`You have exceeded the ${MAX_WINDOW_REQUEST_COUNT} requests in ${WINDOW_SIZE_IN_MINS} minutes limit!`)
-            }        
-            else{
-                let lastRequestLog = data[data.length - 1]
-                console.log("Last Request Log: ", lastRequestLog)
-
-                let potentialCurrentWindowIntervalStartTimeStamp = moment().subtract(WINDOW_LOG_INTERVAL_IN_MINS, 'minutes').unix();                
-
-                const newData = []
-                let newRecord = {}
-                if(lastRequestLog.requestTimeStamp>potentialCurrentWindowIntervalStartTimeStamp){                    
-                    newRecord = {
-                        requestTimeStamp: lastRequestLog.requestTimeStamp,
-                        requestCount: lastRequestLog.requestCount + 1
-                    }                                        
+            let totalWindowRequestsCount = data.tokensInUse || 0;
+                        
+            if(windowStartTimestamp<requestsStartTimeBasedOnRecord){
+                // no increment in tokens
+                if(totalWindowRequestsCount>=MAX_TOKENS_ALLOWED){
+                    res.status(429).send(`The system has exceeded the max ${MAX_TOKENS_ALLOWED} tokens limit. Wait for ${TOKENS_FILLED_PER_IN_SECONDS} seconds to refill the tokens.`)
                 }
-                else{                    
-                    newRecord = {
-                        requestTimeStamp: moment().unix(),
-                        requestCount: 1
-                    }                                        
-                }   
-                newData.push(newRecord);
-                await redisClient.set(req.ip, JSON.stringify(newData));
-                next();                          
+                else{
+                    let newRecord = {
+                        requestTimeStamp: requestsStartTimeBasedOnRecord,
+                        tokensInUse: totalWindowRequestsCount+1,
+                      }
+                    await redisClient.set(TOKEN_BUCKET_REDIS_KEY, JSON.stringify(newRecord));
+                    next();
+                }
             }
+            else{
+                // refill the token 
+                let amountOfTokensToBeFilled = (Math.floor((windowStartTimestamp - requestsStartTimeBasedOnRecord)/TOKENS_FILLED_PER_IN_SECONDS)+1)*TOKENS_TO_BE_FILLED;
+                let tokensUsed = Math.max(totalWindowRequestsCount-amountOfTokensToBeFilled, 1);
+                
+                let newRecord = {
+                    requestTimeStamp: currentRequestTime.unix(),
+                    tokensInUse: tokensUsed,
+                  }
+                
+                await redisClient.set(TOKEN_BUCKET_REDIS_KEY, JSON.stringify(newRecord));
+                next();
+            }            
         }
     }
     catch(err){
@@ -84,5 +82,5 @@ const customRateLimiting = async (req, res, next) =>{
 }
 
 module.exports = {
-    customRateLimiting: customRateLimiting
+    customRedisTokenBucketRateLimiting: customRedisTokenBucketRateLimiting
 };
